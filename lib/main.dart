9642bd9effd78d389b1f14c41a4f3bba31f0ad42
al Mohamady <al.Mohamady47@gmail.com>
2019-09-24 13:06:32 +0200

    Creating UI 01

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:students_list/screens/students_list.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "Students List",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.cyan
+      ),
+      home: StudentsList(),
+    );
+  }
+}
