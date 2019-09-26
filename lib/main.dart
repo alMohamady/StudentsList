6b1ead6e894c4f25aa6d0a8e3abd9ba86ffeb448
al Mohamady <al.Mohamady47@gmail.com>
2019-09-26 12:09:06 +0200

    Asynchronous Future API, Await, Async

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:students_list/screens/students_list.dart';
+import 'dart:async';
 
 
 void main() {
-  runApp(MyApp());
+  //runApp(MyApp());
+  print ("Start the Application");
+  getFileContent();
+  print ("End the Application");
 }
 
+/*getFileContent() async{
+  String fileContent = await downloadFile();
+  print(fileContent);
+}*/
+
+getFileContent() {
+  Future<String> fileContent =  downloadFile();
+  fileContent.then((resut){
+    print(resut);
+  });
+}
+
+Future<String> downloadFile(){
+
+  Future<String> content = Future.delayed(Duration(seconds: 6), () {
+       return "Internet File Content";
+  });
+
+  return content;
+}
 
 class MyApp extends StatelessWidget {
 
